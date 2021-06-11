376161ffcd5d0088585d3a80bb3fe3a2a2c928d6
yasirsoleh <yasirsoleh@gmail.com>
2021-06-11 11:25:58 +0800

    finish v3?

--- resources/views/Delivery/show.blade.php
+++ resources/views/Delivery/show.blade.php
@@ -10,18 +10,18 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="overflow-x-auto">
-                        <label class="label" for="id">Pick Up ID</label>
-                        <input type="text" name="id" placeholder="Pick Up ID" class="input input-bordered" value="{{ $pick_up->id }}" disabled>
+                        <label class="label" for="id">Delivery ID</label>
+                        <input type="text" name="id" placeholder="Delivery ID" class="input input-bordered" value="{{ $delivery->id }}" disabled>
                         <label class="label" for="address">Address</label>
-                        <textarea class="textarea textarea-bordered min-w-full" name="address" style="min-height:100px" placeholder="Address" disabled>{{ $pick_up->address }}</textarea>
+                        <textarea class="textarea textarea-bordered min-w-full" name="address" style="min-height:100px" placeholder="Address" disabled>{{ $delivery->address }}</textarea>
                         <label class="label" for="status">Status</label>
-                        @if ($pick_up->status == 'waiting_rider')
+                        @if ($delivery->status == 'waiting_rider')
                             <span>Waiting Rider</span>
-                        @elseif ($pick_up->status == 'picking_up')
-                            <span>Picking Up</span>
-                        @elseif ($pick_up->status == 'completed')
+                        @elseif ($delivery->status == 'delivering')
+                            <span>Delivering</span>
+                        @elseif ($delivery->status == 'completed')
                             <span>Completed</span>
-                        @elseif ($pick_up->status == 'failed')
+                        @elseif ($delivery->status == 'failed')
                             <span>Failed</span>
                         @endif
                         
